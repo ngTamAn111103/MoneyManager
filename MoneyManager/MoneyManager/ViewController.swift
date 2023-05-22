7d1bb0664a02788c97bb8a752b30727bf6e6cd00
ngTamAn111103 <ng.t.ann2003@gmail.com>
2023-05-22 19:23:30 +0700

    Tạo project

---
+++ MoneyManager/MoneyManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyManager
+//
+//  Created by TAMAN on 22/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
